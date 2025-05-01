6c7e8b372a6af0928c499c53bb52b41dcfc7a780
jmphilips <jmphilips@users.noreply.github.com>
2025-05-01 13:40:53 +0000

    deploy: 6d66937187f12918ed59556ee5fb9d6d2f7b7b5b

--- components/ColorSwatchGrid/ColorSwatchGrid.css.d.ts
+++ components/ColorSwatchGrid/ColorSwatchGrid.css.d.ts
@@ -1,4 +1,7 @@
 export declare const grid: string;
+export declare const rowWrapper: string;
+export declare const rowHeading: string;
+export declare const row: string;
 export declare const swatch: string;
 export declare const preview: string;
 export declare const label: string;
--- components/ColorSwatchGrid/ColorSwatchGrid.d.ts
+++ components/ColorSwatchGrid/ColorSwatchGrid.d.ts
@@ -1,3 +1,13 @@
-export declare const ColorSwatchGrid: ({ colors }: {
+type ColorSwatchGridProps = {
+    colors: {
+        [group: string]: {
+            [shade: string]: string;
+        };
+    };
+};
+export declare const ColorSwatchRow: ({ groupName, colors, }: {
+    groupName: string;
     colors: Record<string, string>;
 }) => import("react/jsx-runtime").JSX.Element;
+export declare const ColorSwatchGrid: ({ colors }: ColorSwatchGridProps) => import("react/jsx-runtime").JSX.Element;
+export {};
